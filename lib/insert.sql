49ee578a24fbc06011383a02ddf86318a7756c8b
Stephen <stevriss22@gmail.com>
2021-11-04 11:57:27 -0400

    commit tree 4a9dcbc09a7bd11f355ea6e6f00c4ed1e8b326f0

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex) VALUES ('Mr.Chocolate', 10, 'M');
+INSERT INTO bears(name,age,sex) VALUES ('Rowdy', 8, 'M');
+INSERT INTO bears(name,age,sex) VALUES ('Tabitha', 9, 'F');
+INSERT INTO bears(name,age,sex) VALUES ('Sergeant Brown', 4, 'F');
+INSERT INTO bears(name,age,sex) VALUES ('Melissa', 7, 'F');
+INSERT INTO bears(name,age,sex) VALUES ('Grinch', 3, 'M');
+INSERT INTO bears(name,age,sex) VALUES ('Wendy', 6, 'F');
+INSERT INTO bears(name,age,sex) VALUES (NULL, 2, 'M');
